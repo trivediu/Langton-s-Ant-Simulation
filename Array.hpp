c1e6cae1e067b972217bdb3ab8697acd4d0af709
trivediu <31199718+trivediu@users.noreply.github.com>
2017-10-10 03:01:23 -0700

    Array.hpp

---
+++ Array.hpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+Author:        Uday S. Trivedi
+
+Description:   This header file contains the function
+prototypes for creating and destroying the 2D array. 
+
+For full details on what each function does, input argument,
+restrictions, and returns please refer to the
+Array.cpp file.
+
+******************************************************************************/
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+char **createArray(int, int);
+void deleteArray(char **, int, int);
+
+#endif
+
+
+
+
+
+
+
+
+
+//Jay Swaminarayan
